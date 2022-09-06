b9a77918785742053ee6b11d9d5e564cad7d23c4
Dhiradj Tangali <90622013+DhiradjT@users.noreply.github.com>
2022-09-06 16:41:24 +0200

    Opdracht 1t/m5 gemaakt van php if else statements

---
+++ if-sd21-p5-php-huiswerk-DhiradjT/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opdrachten Hoofdstuk 4 Dhiradj Tangali</title>
+</head>
+<body>
+    <h1>Hoofdstuk 4 | Opdracht 1 t/m 5</h1>
+    <?php
+    
+    //Opdracht 1 [voltooid]
+    // $timeOfDay = date("H");
+
+    // if ($timeOfDay >= 6 && $timeOfDay <= 12) {
+    //     echo "Het is ochtend";
+    // } else if ($timeOfDay >= 12 && $timeOfDay <= 18 ) {
+    //     echo "Het is middag";
+    // } else if ($timeOfDay >= 18 && $timeOfDay <= 24) {
+    //     echo "Het is avond";
+    // } else if ($timeOfDay <= 0 && $timeOfDay >= 24) {
+    //     echo "Het is nacht";
+    // }
+
+    //Opdracht 2 [voltooid]
+//     $time = date("H");
+
+//     $result = match (true) {
+//     $time >= 6 && $time <= 12 => 'Het is ochtend',
+//     $time >= 12 && $time <= 18 => 'Het is middag',
+//     $time >= 18 && $time <= 24 => 'Het is avond',
+//     $time <= 0 && $time >= 24 => 'Het is nacht',
+//     default => 'Het is nacht',
+// };
+
+// var_dump($result);
+
+
+
+    //Opdracht 3 [voltooid]
+
+    // $x = 6;
+    // $y = 12;
+     
+    // if ($y > $x) {
+    //     echo $y * 2 + $x;
+    // } else if ($x > $y) {
+    //     echo $x * 2 + $y;
+    // } 
+
+    
+
+    //Opdracht 4 [voltooid]
+    //  $priceExBTW = 200 ;
+
+    //  if ($priceExBTW > 150) {
+    //     echo $priceExBTW * 1.19; 
+    //  } else if ($priceExBTW < 55) {
+    //     echo $priceExBTW * 1.11;
+    //  } else if ($priceExBTW <= 55 && $priceExBTW <= 150) {
+    //     echo $priceExBTW * 1.16;
+    //  }
+
+     //Opdracht 5 [voltooid]
+    //  function check($number){
+    //     if($number % 2 == 0){
+    //         echo $number .  "is even"; 
+    //     }
+    //     else{
+    //         echo $number . "is oneven";
+    //     }
+    // }
+      
+    // Aanroepen
+    // $number = 27;
+    // check($number)
+    
+    ?>
+</body>
+</html>
